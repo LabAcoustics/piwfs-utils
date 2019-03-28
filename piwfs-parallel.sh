cb29236d3926a0bc113f0af8b5a136dd876048b6
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-03-28 17:31:10 +0100

    More great scripts added

--- piwfs-parallel.sh
+++ piwfs-parallel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cmd=$1
 addr_ar=($(cat slave_ip.txt))
 for it in $(seq 0 $((${#addr_ar[@]}-1))); do
   hostn=slave$((it + 1))
   address=${addr_ar[it]}
   echo "Connection to $hostn at ip: $address"
-  rsync -P $@ pi@$address:/home/pi
+  ssh pi@$address "$cmd"&
 done
