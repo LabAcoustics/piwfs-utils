b6cb7b6943a04539e3988394bf4ed848210b233d
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-12-14 17:39:48 +0100

    Choose interface

--- piwfs-connect.sh
+++ piwfs-connect.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-device=$(ip addr | grep ": wl" | cut -d':' -f 2 | tail -c+2)
+#device=$(ip addr | grep ": wl" | cut -d':' -f 2 | tail -c+2)
+device="$1"
 subnet=$(ip addr show dev $device | grep "inet " | cut -d' ' -f 6)
 addr=$(nmap -sn $subnet -oG - | sed "/^#/d" | cut -d' ' -f 2)
 
 addr_ar=()
 for address in $addr; do
-  hostn=$(ssh -o ConnectTimeout=2 pi@$address 'hostname')
+  hostn=$(ssh -o ConnectTimeout=2 -o BatchMode=yes -o StrictHostKeyChecking=no pi@$address 'hostname')
   if [[ $? == 0 ]]; then
     echo "Connected to $hostn at ip: $address"
     if [[ $hostn == master ]]; then
--- piwfs-parunmaswa.sh
+++ piwfs-parunmaswa.sh
@@ -4,6 +4,6 @@ while true; do
     ./piwfs-parallel.sh "$@"
     sleep 0.5
 
-    ssh pi@$(cat ./master_ip.txt) "./piwfs/target/debug/piwfs master --time 10"
-    sleep 0.5
+    ssh pi@$(cat ./master_ip.txt) "./piwfs/target/debug/piwfs master --time 70"
+    sleep 2
 done
